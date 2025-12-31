e9052735efeaabe2c5c1b0b7f8ea077a03d88e45
Vighnesh Radhakrishnan <vighnesh7radhakrishnan@gmail.com>
2026-01-01 02:24:17 +0530

    added new features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,6 @@ export default function Home() {
   return (
     <>
       <h1>Home</h1>
-      <Link href="/product">Products</Link>
-      <Link href="/docs">Docs</Link>
-      <Link href="/login">login</Link>
-      <Link href="/profile">profile</Link>
     </>
   );
 }

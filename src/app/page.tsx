58199811e67d84b956024b350bd485454a1e52c1
Vighnesh Radhakrishnan <vighnesh7radhakrishnan@gmail.com>
2025-12-31 21:21:14 +0530

    link added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
 export default function Home() {
-  return <h1>Home!</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <Link href="/product">Products</Link>
+      <Link href="/docs">Docs</Link>
+      <Link href="/login">login</Link>
+      <Link href="/profile">profile</Link>
+    </>
+  );
 }
